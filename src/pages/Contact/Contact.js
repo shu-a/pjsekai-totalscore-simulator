0e5060cde097c88a74ede8d182eb3d879036e044
shu <78002112+shu-a@users.noreply.github.com>
2022-07-15 16:54:12 +0900

    임시 페이지 추가

---
+++ src/pages/Contact/Contact.js
@@ -0,0 +1,11 @@
+import React, { Fragment } from 'react';
+
+export default function Contact() {
+  const content = 'Contact';
+
+  return (
+    <Fragment>
+      {content}
+    </Fragment>
+  );
+}
---
+++ src/pages/Instruction/Instruction.js
@@ -0,0 +1,11 @@
+import React, { Fragment } from 'react';
+
+export default function Instruction() {
+  const content = '사이트 사용 설명서';
+
+  return (
+    <Fragment>
+      {content}
+    </Fragment>
+  );
+}

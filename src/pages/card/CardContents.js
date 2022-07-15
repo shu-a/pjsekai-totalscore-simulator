c2f44f2fe1bf58e6eaad4fc095839801a7005199
shu <78002112+shu-a@users.noreply.github.com>
2022-07-15 11:49:13 +0900

    변수 등 약간의 수정, 일부 구조 변경
    핸들러에서 다음 동작해야 하는 함수를 직접 호출하는 방식에서 useEffect를 사용하는 방식으로 수정

--- src/pages/card/CardContents.js
+++ src/pages/card/CardContents.js
@@ -1,4 +1,4 @@
-import React, { useState, Fragment } from 'react';
+import React, { useState, Fragment, useEffect } from 'react';
 import MakeMemberCard from '../../components/MakeMemberCard';
 import MenuItem from '@mui/material/MenuItem';
 import MakeFormSelect from '../../components/MakeFormSelect';
@@ -6,80 +6,82 @@ import MakeTextField from '../../components/MakeTextField';
 
 export default function CardContents(props) {
   const [disabled, setDisabled] = useState(true);
-  // 값 및 핸들러
+  const changeDisabled = (affiliation) => {
+    if (affiliation) {
+      setDisabled(false);
+      setHelperText('');
+    }
+    else {
+      setDisabled(true);
+      setHelperText(defaultHelperText);
+    }
+  }
   const [attr, setAttr] = useState('');
   const [affiliation, setAffiliation] = useState('');
   const [team, setTeam] = useState('');
   const [character, setCharacter] = useState('');
   const [rarities, setRarities] = useState('');
-  const HandlerSelectAttr = (event) => {
+  const handleSelectAttr = (event) => {
     setAttr(event.target.value);
   }
-  const HandlerSelectAffiliation = (event) => {
+  const handleSelectAffiliation = (event) => {
     setAffiliation(event.target.value);
-    enableCharacter(event.target.value);
-    if (event.target.value === 'piapro')
-      setTeam('');
-    else
-      setTeam(event.target.value);
+    setCharacter('');
   }
-  const HandlerSelectTeam = (event) => {
+  const handleSelectTeam = (event) => {
     setTeam(event.target.value);
   }
-  const HandlerSelectRarities = (event) => {
+  const handleSelectRarities = (event) => {
     setRarities(event.target.value);
   }
-  const HandlerSelectCharacter = (event) => {
+  const handleSelectCharacter = (event) => {
     setCharacter(event.target.value);
   }
 
-  // const _characterList = [];
-  const [__characterList, __setCharacterList] = useState([]);
-  const enableCharacter = (value) => {
-    setCharacter('');
-    if (value)
-      setDisabled(false);
+  const [charactList, setCharacterList] = useState([]);
+  useEffect(() => {
+    changeDisabled(affiliation);
+    if (affiliation === 'piapro')
+      setTeam('');
     else
-      setDisabled(true);
-
-    const _characterList = props.characterList.map((c) => {
-      if (value === (c.unit))
+      setTeam(affiliation);      
+    setCharacterList(props.characterList.map((c) => {
+      if (affiliation === (c.unit))
         return <MenuItem key={c.id} value={c.id}>{c.fullName}</MenuItem>;
       else
         return false;
-    });
-    __setCharacterList(_characterList);
-  }
+    }));
+  }, [affiliation, props.characterList]);
 
-  const _affiliationList = props.teamList.map((c) =>
-  // ***
+  const affiliationList = props.teamList.map((c) =>
     <MenuItem key={c.seq} value={c.unit} unit={c.unit}>{c.unitName}</MenuItem>
   );
-  const _attrList = props.attrList.map((c) =>
+  const attrList = props.attrList.map((c) =>
     <MenuItem key={c.unit} value={c.unit}>{c.unitName}</MenuItem>
   );
-  const _teamList = props.teamList.map((c) => {
+  const teamList = props.teamList.map((c) => {
     if (Number(c.seq) !== 1)
       return <MenuItem key={c.unit} value={c.unit}>{c.unitName}</MenuItem>
     else
       return false
   });
-  const _raritiesList = props.raritiesList.map((c) =>
+  const raritiesList = props.raritiesList.map((c) =>
     <MenuItem key={c.seq} value={c.seq} >{c.cardRarityType.split('_')[1]}</MenuItem>
   );
 
   const makeFormSelect = [];
-
+  const defaultHelperText = '소속을 선택하세요';
+  const [helperText, setHelperText] = useState(defaultHelperText);
   const makeFormSelectContents = [
-    { sx: { m: 1, width: 256 }, id: 'attr', label: '속성', value: attr, handler: HandlerSelectAttr, selectList: _attrList },
-    { sx: { m: 1, width: 120 }, id: 'affiliation', label: '소속', value: affiliation, handler: HandlerSelectAffiliation, selectList: _affiliationList },
-    { sx: { m: 1, width: 120 }, id: 'team', label: '팀', value: team, handler: HandlerSelectTeam, selectList: _teamList, disabled: disabled },
-    { sx: { m: 1, width: 120 }, id: 'rarities', label: '성급', value: rarities, handler: HandlerSelectRarities, selectList: _raritiesList },
-    { sx: { m: 1, width: 120 }, id: 'character', label: '캐릭터명', value: character, handler: HandlerSelectCharacter, selectList: __characterList, disabled: disabled, helperText: '팀을 선택하세요.' }
+    { sx: { m: 1, width: 256 }, id: 'attr', label: '속성', value: attr, handler: handleSelectAttr, selectList: attrList },
+    { sx: { m: 1, width: 120 }, id: 'affiliation', label: '소속', value: affiliation, handler: handleSelectAffiliation, selectList: affiliationList },
+    { sx: { m: 1, width: 120 }, id: 'team', label: '팀', value: team, handler: handleSelectTeam, selectList: teamList, disabled: disabled, helperText: helperText },
+    { sx: { m: 1, width: 120 }, id: 'rarities', label: '성급', value: rarities, handler: handleSelectRarities, selectList: raritiesList },
+    { sx: { m: 1, width: 120 }, id: 'character', label: '캐릭터명', value: character, handler: handleSelectCharacter, selectList: charactList, disabled: disabled, helperText: helperText }
   ];
   const makeFormSelectlist = makeFormSelectContents.map((c) =>
     <MakeFormSelect key={c.id} id={props.title + '_' + c.id} sx={c.sx} label={c.label} inputLabel={c.label} value={c.value} handler={c.handler}
-    selectList={c.selectList} disabled={c.disabled} helperText={c.makeFormSelectlist} />
+      selectList={c.selectList} disabled={c.disabled} helperText={c.helperText} />
   );
   makeFormSelect.push(makeFormSelectlist);
 
@@ -92,8 +94,8 @@ export default function CardContents(props) {
     <MakeTextField key={c.id} id={props.title + '_' + c.id} label={c.label} defaultValue='' type={c.type} sx={c.sx} />
   );
   makeFormSelect.push(makeTextFieldList);
-  const cardContents = [];
-  cardContents.push(<MakeMemberCard key='MakeMemberCard' makeFormSelect={makeFormSelect} title={props.title} border={props.border} color={props.color} />);
+  
+  const cardContents = <MakeMemberCard key='MakeMemberCard' makeFormSelect={makeFormSelect} title={props.title} border={props.border} color={props.color} />;
   return (
     <Fragment>
       {cardContents}

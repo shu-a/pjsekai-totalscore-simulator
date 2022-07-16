d45e45810771b84eb234ece90babab49ff5755b6
shu <78002112+shu-a@users.noreply.github.com>
2022-07-17 01:32:32 +0900

    코드 정리

--- src/components/MakeTextField.js
+++ src/components/MakeTextField.js
@@ -1,4 +1,4 @@
-import React, { Fragment, useEffect, useState } from 'react';
+import React, { Fragment } from 'react';
 import TextField from '@mui/material/TextField';
 
 export default function MakeTextField(props) {

38cc49290be474c81c891399046613aa84027a7b
shu <78002112+shu-a@users.noreply.github.com>
2022-07-18 06:30:11 +0900

    스타일 수정

--- src/pages/Card/CardSet.js
+++ src/pages/Card/CardSet.js
@@ -19,18 +19,18 @@ export default function Card() {
   useEffect(() => {
     getRaritiesList().then((resData) => setRaritiesList(resData));
   }, []);
-  
+
   const cardInfo = [
-    { title: 'Reader', idx: 1, color: '#ed5fab', border: 2 },
-    { title: 'SubReader', idx: 2, color: '#6fc6bd', border: 2 },
-    { title: 'Member1', idx: 3, color: '#444564', border: 2 },
-    { title: 'Member2', idx: 4, color: '#444564', border: 2 },
-    { title: 'Member3', idx: 5, color: '#444564', border: 2 }
+    { title: 'Reader', idx: 1, color: '#ed5fab', border: 2, pb: 2 },
+    { title: 'SubReader', idx: 2, color: '#6fc6bd', border: 2, pb: 2 },
+    { title: 'Member1', idx: 3, color: '#444564', border: 2, pb: 2 },
+    { title: 'Member2', idx: 4, color: '#444564', border: 2, pb: 2 },
+    { title: 'Member3', idx: 5, color: '#444564', border: 2, pb: 2 }
   ];
-  
+
   const card = cardInfo.map((c) =>
     <CardContents key={c.idx} teamList={teamList} attrList={attrList} characterList={characterList} raritiesList={raritiesList}
-    title={c.title} border={c.border} color={c.color} />
+      title={c.title} border={c.border} color={c.color} />
   );
 
   return (
--- src/pages/SekaiViewerLink.js
+++ src/pages/SekaiViewerLink.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import Button from '@mui/material/Button';
 
+const style1 = {
+  backgroundColor: '#4ca692',
+  '&:hover': { backgroundColor: '#3b8071' }
+}
+// const style2 = {
+//   backgroundColor: '#ffffff',
+//   color: '#3b8071',
+//   '&:hover': { color: '#ffffff', backgroundColor: '#3b8071' }
+// }
+
 function SekaiViewerLink() {
   return(
     <div>
-      <Button variant="contained" sx={{ backgroundColor: '#4ca692', '&:hover': { backgroundColor: '#3b8071' }}} onClick={e => {
+      <Button variant="contained" sx={style1} onClick={e => {
         e.preventDefault();
         window.open('https://sekai.best/card', '_blank')
         console.log('클릭');

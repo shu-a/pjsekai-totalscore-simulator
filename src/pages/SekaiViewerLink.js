240862fb83bdbfdeaf73b5961308661b6bf6d5b9
shu <78002112+shu-a@users.noreply.github.com>
2022-07-19 02:13:56 +0900

    스타일 변경

--- src/pages/SekaiViewerLink.js
+++ src/pages/SekaiViewerLink.js
@@ -1,20 +1,24 @@
 import React from 'react';
 import Button from '@mui/material/Button';
 
-const style1 = {
-  backgroundColor: '#4ca692',
-  '&:hover': { backgroundColor: '#3b8071' }
-}
+// const style1 = {
+//   backgroundColor: '#4ca692',
+//   '&:hover': { backgroundColor: '#3b8071' }
+// }
 // const style2 = {
 //   backgroundColor: '#ffffff',
 //   color: '#3b8071',
 //   '&:hover': { color: '#ffffff', backgroundColor: '#3b8071' }
 // }
+const style3 = {
+  backgroundColor: '#00b3a4',
+  '&:hover': { backgroundColor: '#008075' }
+}
 
 function SekaiViewerLink() {
   return(
     <div>
-      <Button variant="contained" sx={style1} onClick={e => {
+      <Button variant="contained" sx={style3} onClick={e => {
         e.preventDefault();
         window.open('https://sekai.best/card', '_blank')
       }}>

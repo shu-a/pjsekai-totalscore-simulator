138289bf397836575f0be01d824043a824b18e1b
shu <78002112+shu-a@users.noreply.github.com>
2022-07-20 02:40:54 +0900

    NotFound 추가

---
+++ src/NotFound.js
@@ -0,0 +1,12 @@
+import { Box } from '@mui/system';
+import React from 'react';
+
+export default function NotFound() {
+  return (
+    <Box component="div">
+      <Box component="div" justifyContent="center" alignItems="center" minHeight="80vh" display="flex" sx={{ fontSize: 30 }}>
+        존재하지 않는 페이지입니다.
+      </Box>
+    </Box>
+  );
+}

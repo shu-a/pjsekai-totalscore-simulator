9f889e8d541222b1b2440addb33f18884e9dc768
shu <78002112+shu-a@users.noreply.github.com>
2022-07-19 22:08:31 +0900

    코드 정리

--- src/components/MakeFormSelect.js
+++ src/components/MakeFormSelect.js
@@ -17,28 +17,28 @@ export default function MakeFormSelect(props) {
   */
   return (
     <Fragment>
-    <FormControl
-      variant="standard"
-      key={props.id}
-      sx={props.sx}
-    >
-      <InputLabel>{props.inputLabel}</InputLabel>
-      <Select
-        label={props.label}
-        labelId={props.id}
-        value={props.value}
-        onChange={props.handler}
-        name={props.id}
-        inputProps={{
-          id: props.id,
-          readOnly: props.readonly
-        }}
-        sx={props.selectSx}
+      <FormControl
+        variant="standard"
+        key={props.id}
+        sx={props.sx}
       >
-        {props.selectList}
-      </Select>
-      <FormHelperText>{props.helperText}</FormHelperText>
-    </FormControl>
+        <InputLabel>{props.inputLabel}</InputLabel>
+        <Select
+          label={props.label}
+          labelId={props.id}
+          value={props.value}
+          onChange={props.handler}
+          name={props.id}
+          inputProps={{
+            id: props.id,
+            readOnly: props.readonly
+          }}
+          sx={props.selectSx}
+        >
+          {props.selectList}
+        </Select>
+        <FormHelperText>{props.helperText}</FormHelperText>
+      </FormControl>
     </Fragment>
   );
 }

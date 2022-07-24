b854501cd5b8d0fc2297ab53f0e83fd34e3597d1
shu <78002112+shu-a@users.noreply.github.com>
2022-07-24 18:37:47 +0900

    Update Index.js

--- src/pages/About/Index.js
+++ src/pages/About/Index.js
@@ -1,7 +1,7 @@
 import { Link } from '@mui/material';
 import { Box } from '@mui/system';
 import React from 'react';
-import GitHubIcon from '@mui/icons-material/GitHub';
+// import GitHubIcon from '@mui/icons-material/GitHub';
 import MailIcon from '@mui/icons-material/Mail';
 
 export default function AboutIndex() {
@@ -14,11 +14,11 @@ export default function AboutIndex() {
           <Box sx={{ fontSize: 30 }}>
             About
           </Box>
-          <Box sx={{ mt: 1 }}>
+          {/* <Box sx={{ mt: 1 }}>
             <Link rel="noopener" target="_blank" href="https://github.com/shu-a" underline="none" color="rgba(0, 0, 0, 0.6)">
               <GitHubIcon fontSize='large' sx={{ position: 'relative', top: '8px' }} /> <Box component="span" sx={{ fontSize: 25 }}>GitHub</Box>
             </Link>
-          </Box>
+          </Box> */}
           <Box sx={{ mt: 1 }}>
             <Link rel="noopener" target="_blank" href="mailto:pjsekaiscore@shu-a.app" underline="none" color="rgba(0, 0, 0, 0.6)" sx={{ verticalAlign: 'middle' }}>
               <MailIcon fontSize='large' sx={{ position: 'relative', top: '8px' }} /> <Box component="span" sx={{ fontSize: 25 }}>Mail</Box>
